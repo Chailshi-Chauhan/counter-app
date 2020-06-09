2d25f8e24500e8477d653cdf2d1606261f54f39f
Chailshi chauhan <chailshichauhan2000@gmail.com>
2020-06-09 15:59:12 +0530

    Updates

---
+++ precache-manifest.e70b1044e4851ea268a693cb1be25e6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ed06b1a56ee2c595b303a70267b835",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "17dc0f5b0424c439853e",
+    "url": "/counter-app/static/css/main.7f2e2067.chunk.css"
+  },
+  {
+    "revision": "85516dd54298450fca5c",
+    "url": "/counter-app/static/js/2.fb994f7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter-app/static/js/2.fb994f7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17dc0f5b0424c439853e",
+    "url": "/counter-app/static/js/main.5de80c94.chunk.js"
+  },
+  {
+    "revision": "99705780ce1b7e6bcb14",
+    "url": "/counter-app/static/js/runtime-main.3958c4e3.js"
+  }
+]);
